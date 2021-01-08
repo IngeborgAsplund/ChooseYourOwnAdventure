71f1c8726d27d1af4a0600d9d7d837b23f77029f
Hortencya <ingeborg.asplund@gmail.com>
2021-01-08 12:51:17 +0100

    enum implemented
    Now the bug that crashed the game when loading a game when application is running is fixed using an enum for the various states of the game

--- Adenture on Glimmer Island/Adenture on Glimmer Island/Game.cpp
+++ Adenture on Glimmer Island/Adenture on Glimmer Island/Game.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"game.h"
+GameState state;
 //The function through which the game is run it contains the main gameloop. As the game grows stuff done here will be 
 //split up on multiple classes
 void Game::Run()
@@ -9,108 +10,18 @@ void Game::Run()
 	std::cout << "Debugging Locations"<<"\n";
 	gameData.DebugLocations();
 	std::regex nameRegex("%%Name%%");
-	ShowMenue(nameRegex);
+	isRunning = true;	
+	state = GameState::MENUE;
 	//start game loop
 	while (isRunning)
-	{
-		//check that player. currentlocation is not a nullpointer
-		if(player.currentLocation==nullptr)
-		{
-			std::cout<<"\n"<<"the location currently set as current location do not exist, please make sure all of the locations in the choices is valid";
-			isRunning = false;//is running is false
-			return;//return to the top breaking the while loop
-		}
-		
-		std::string roomDescription = std::regex_replace(player.currentLocation->description, nameRegex, player.name);
-		std::cout << roomDescription<<"\n\n";		
-		//check so that we havent added the id to the visiedlocations vector
-		if (!player.AlreadyVisited(player.currentLocation->id))
-		{
-			//if not see if there are any items to pick up from the location
-			if (player.currentLocation->items.size() > 0)
-			{
-				//go through the list of items 
-				for (int i = 0; i < player.currentLocation->items.size(); i++)
-				{
-					//and add them to the player if they are not equal to null
-					if (gameData.GetItemById(player.currentLocation->items[i]) != nullptr)
-					{
-						player.AddItem(player.currentLocation->items[i], 1);
-						std::cout << "You picked up " << player.currentLocation->items[i] << "\n";
-					}
-				}
-			}
-			if(player.currentLocation->negativeItems.size()>0)
-			{
-				for(int k = 0; k< player.currentLocation->negativeItems.size(); k++)
-				{
-					if (gameData.GetItemById(player.currentLocation->negativeItems[k]) != nullptr)
-					{
-						player.RemoveItem(player.currentLocation->negativeItems[k], 1);
-						std::cout << "\nUsed up " << player.currentLocation->negativeItems[k]<<"\n";
-					}
-				}
-			}
-			player.visitedLocations.push_back(player.currentLocation->id);
-		}
-		//check if choices equals zero
-		if(player.currentLocation->choices.size()==0)
-		{
-			//if so remark the game have ended
-			std::cout << "\n" << "The story of the Adventure of Glimmer Island has come to an end.";
-			std::cout << "\nYour moves this game was " << player.moves << "\n";//write out the current number of moves.
-			std::cout << "\n" << "You visited the following locations durning the game\n";
-			if(player.visitedLocations.size()>0)
-			{
-				for(int j = 0;j<player.visitedLocations.size();j++)
-				{
-					std::cout << player.visitedLocations[j]<<"\n";
-				}
-			}
-
-			isRunning = false;//set isrunning false
-			return;// return back to top, exiting the loop
-		}
-		//write out all of the choices for the current location
-		std::vector<LocationChoice> available = FindAvailableChoices();
-		for(int i =0;i<available.size();i++)
-		{
-			std::cout <<i+1<<". "<< std::regex_replace(available[i].choiceDescription,nameRegex,player.name)<<"\n";			
-		}
-		
-		std::cout << "Menue [m]\n"<<"Inventory[i]\n";//write out input call pr the menue
-		isRunning = player.CheckHungerStatus(nameRegex);
-		//prompt for input
-		std::string prompt= "What do %%Name%% want to do?\nPick their choice through printing the number of the choice and press enter\n";
-		std::string promptstring = std::regex_replace(prompt, nameRegex, player.name);
-		std::cout << promptstring;
-		SaveGame();
-		//read input
-		std::string inputstring;
-		int input;
-		std::cin >> inputstring;
-		if(inputstring[0]=='m')
-		{
-			ShowMenue(nameRegex);
-		}
-		if(inputstring[0]=='i')
-		{
-			ShowvInventory();
-		}
-		else
-		{
-			
-			input = CastStringToInt(inputstring);
-			//if input is bigger than zero but less than or equal to the max amount of choices
-			if (input > 0 && input <= available.size())
-			{				
-				player.currentLocation = gameData.GetLocationWithId(available[input - 1].locationID);
-				player.moves++;//increase player moves
-				player.satation -= 5;
-			}
-			
-		}
-		
+	{		
+		if( state==GameState::MENUE)
+			ShowMenue(nameRegex);			
+		if(state==GameState::RUNNING)
+			RunMainLoop(nameRegex);			
+		if(state== GameState::INVENTORY)
+			ShowvInventory();			
+				
 	}
 	
 }
@@ -144,6 +55,107 @@ std::vector<LocationChoice> Game::FindAvailableChoices()
 	}
 	return choicesToPresent;
 }
+void Game::RunMainLoop(std::regex inRegex)
+{
+	//check that player. currentlocation is not a nullpointer
+	if (player.currentLocation == nullptr)
+	{
+		std::cout << "\n" << "the location currently set as current location do not exist, please make sure all of the locations in the choices is valid";
+		isRunning = false;//is running is false
+		return;//return to the top breaking the while loop
+	}
+
+	std::string roomDescription = std::regex_replace(player.currentLocation->description, inRegex, player.name);
+	std::cout << roomDescription << "\n\n";
+	//check so that we havent added the id to the visiedlocations vector
+	if (!player.AlreadyVisited(player.currentLocation->id))
+	{
+		//if not see if there are any items to pick up from the location
+		if (player.currentLocation->items.size() > 0)
+		{
+			//go through the list of items 
+			for (int i = 0; i < player.currentLocation->items.size(); i++)
+			{
+				//and add them to the player if they are not equal to null
+				if (gameData.GetItemById(player.currentLocation->items[i]) != nullptr)
+				{
+					player.AddItem(player.currentLocation->items[i], 1);
+					std::cout << "You picked up " << player.currentLocation->items[i] << "\n";
+				}
+			}
+		}
+		if (player.currentLocation->negativeItems.size() > 0)
+		{
+			for (int k = 0; k < player.currentLocation->negativeItems.size(); k++)
+			{
+				if (gameData.GetItemById(player.currentLocation->negativeItems[k]) != nullptr)
+				{
+					player.RemoveItem(player.currentLocation->negativeItems[k], 1);
+					std::cout << "\nUsed up " << player.currentLocation->negativeItems[k] << "\n";
+				}
+			}
+		}
+		player.visitedLocations.push_back(player.currentLocation->id);
+	}
+	//check if choices equals zero
+	if (player.currentLocation->choices.size() == 0)
+	{
+		//if so remark the game have ended
+		std::cout << "\n" << "The story of the Adventure of Glimmer Island has come to an end.";
+		std::cout << "\nYour moves this game was " << player.moves << "\n";//write out the current number of moves.
+		std::cout << "\n" << "You visited the following locations durning the game\n";
+		if (player.visitedLocations.size() > 0)
+		{
+			for (int j = 0; j < player.visitedLocations.size(); j++)
+			{
+				std::cout << player.visitedLocations[j] << "\n";
+			}
+		}
+
+		isRunning = false;//set isrunning false
+		return;// return back to top, exiting the loop
+	}
+	//write out all of the choices for the current location
+	std::vector<LocationChoice> available = FindAvailableChoices();
+	for (int i = 0; i < available.size(); i++)
+	{
+		std::cout << i + 1 << ". " << std::regex_replace(available[i].choiceDescription, inRegex, player.name) << "\n";
+	}
+
+	std::cout << "Menue [m]\n" << "Inventory[i]\n";//write out input call pr the menue
+	isRunning = player.CheckHungerStatus(inRegex);
+	//prompt for input
+	std::string prompt = "What do %%Name%% want to do?\nPick their choice through printing the number of the choice and press enter\n";
+	std::string promptstring = std::regex_replace(prompt, inRegex, player.name);
+	std::cout << promptstring;
+	SaveGame();
+	//read input
+	std::string inputstring;
+	int input;
+	std::cin >> inputstring;
+	if (inputstring[0] == 'm')
+	{
+		state = GameState::MENUE;		
+	}
+	if (inputstring[0] == 'i')
+	{
+		state = GameState::INVENTORY;	
+	}
+	else
+	{
+
+		input = CastStringToInt(inputstring);
+		//if input is bigger than zero but less than or equal to the max amount of choices
+		if (input > 0 && input <= available.size())
+		{
+			player.currentLocation = gameData.GetLocationWithId(available[input - 1].locationID);
+			player.moves++;//increase player moves
+			player.satation -= 5;			
+		}
+
+	}
+
+}
 void Game::ShowMenue(std::regex inRegex)
 {
 	isRunning = false;//temporary shut of the game
@@ -158,22 +170,22 @@ void Game::ShowMenue(std::regex inRegex)
 		std::string  introduce = "\nAfter the ship where %%Name%% worked as a deckhand sunk during a raging storm\n%%Name%% find themselves stranded on the rocky beaches of a tropical island,\nthere %%Name%% have set up a camp on the northern shore.\nWhile waiting or rather hoping for another ship to pass by and pick them up\n %%Name%% are currently doing their best to survive,\nexploring the landscape of the island and collecting whatever resources they can find.\n\n";
 		std::string output = std::regex_replace(introduce, inRegex, player.name);
 		std::cout << output;
-		isRunning = true;
-		return;
+		state = GameState::RUNNING;//set the game state to running
+		
 	}
 	if(input==2)
 	{
 		isRunning = false;
-		return;
+		
 	}
 	if(input==3)
 	{
 		LoadGame();
-		isRunning = true;
+		state = GameState::RUNNING;
 	}
-	else
+	if(input<1||input>3)
 	{
-		std::cout << "\n Faulty input. Please input must range between 1-2";
+		std::cout << "\n Faulty input. Please input must range between 1-3";
 	}
 }
 //when showing the inventory this function is called
@@ -194,7 +206,7 @@ void Game::ShowvInventory()
 	std::cin >> inventoryInput;
 	if (inventoryInput[0] == 'i')
 	{
-		return;//exit this function
+		state = GameState::RUNNING;
 	}
 	else
 	{
--- Adenture on Glimmer Island/Adenture on Glimmer Island/game.h
+++ Adenture on Glimmer Island/Adenture on Glimmer Island/game.h
@@ -1,7 +1,11 @@
 #pragma once
 #include"gameData.h"
 #include"player.h"
-
+//enum describing the various states the game can be in.
+enum class GameState
+{
+	RUNNING,MENUE,INVENTORY
+};
 class Game
 {
 public:
@@ -13,6 +17,7 @@ private:
 	//variables
 	bool isRunning;
 	//functions
+	void RunMainLoop(std::regex inRegex);
 	void ShowMenue(std::regex inRegex);//this is called whenever we want to bring up the games menue, right now it has two options exit and new game
 	void ShowvInventory();
 	std::vector<LocationChoice> FindAvailableChoices();
@@ -22,3 +27,4 @@ private:
 	int CastStringToInt(std::string &castMe);//try and cast the string returning an errormessage to console if failing
 
 };
+
